8346c2929e0fb66b1b4afc2a573e23185f22478b
Tyler Henn <37907509+thennbreak@users.noreply.github.com>
2020-06-11 13:56:51 -0400

    Update PythonPost.py

--- PythonPost.py
+++ PythonPost.py
@@ -1,18 +1,17 @@
-from pythonds.basic.stack import Stack
-
 def postfixEval(postfixExpr):
-    operandStack = Stack()
+    operandStack = []
     tokenList = postfixExpr.split()
 
     for token in tokenList:
         if token in "0123456789":
-            operandStack.push(int(token))
+            operandStack.append(int(token))
         else:
             operand2 = operandStack.pop()
             operand1 = operandStack.pop()
-            result = doMath(token,operand1,operand2)
-            operandStack.push(result)
+            result = doMath(token, operand1, operand2)
+            operandStack.append(result)
     return operandStack.pop()
+
 
 def doMath(op, op1, op2):
     if op == "*":
@@ -23,5 +22,6 @@ def doMath(op, op1, op2):
         return op1 + op2
     else:
         return op1 - op2
+
 
 print(postfixEval('2 3 + 5 1 - *'))
